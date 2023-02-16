9ad7170fdd69ad009f3cd11ef31da44ad1648ab6
zacheusIsrael <segunzacheusi@gmail.com>
2023-02-16 15:48:01 +0100

    Create loader.component.jsx

---
+++ src/components/spin-loader/loader.component.jsx
@@ -0,0 +1,11 @@
+import "./loader.styles.scss";
+
+const Spinner = () => {
+  return (
+    <div className="spinnerOverlay">
+      <div className="spinnerContainer"></div>
+    </div>
+  );
+};
+
+export default Spinner;
