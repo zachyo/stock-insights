8726460ed5a5968e7a59a2d00849d3558cf04453
zacheusIsrael <segunzacheusi@gmail.com>
2023-02-16 15:48:18 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,11 +3,14 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { SearchProvider } from './contexts/searchContext';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <SearchProvider>
+      <App />
+    </SearchProvider>
   </React.StrictMode>
 );
 
