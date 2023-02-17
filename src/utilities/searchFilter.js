8f27fb8908d42ab859a5ebf2f1e43e63783f88fa
zacheusIsrael <segunzacheusi@gmail.com>
2023-02-17 11:45:45 +0100

    Update searchFilter.js

--- src/utilities/searchFilter.js
+++ src/utilities/searchFilter.js
@@ -1,6 +1,6 @@
 export const searchFilter = (searchKey, data) => {
   let newData = data.filter(
-    (each) => each?.name.toLowerCase().indexOf(searchKey.toLowerCase()) === 0
+    (each) => each?.title.toLowerCase().indexOf(searchKey.toLowerCase()) === 0
   );
   return newData;
 };
