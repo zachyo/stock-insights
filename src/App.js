cacc0697d24cec886ea46d317343f5e46eb5bff3
zacheusIsrael <segunzacheusi@gmail.com>
2023-02-17 20:29:31 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,13 +2,16 @@ import "./App.css";
 import { Route, Routes } from "react-router-dom";
 
 import Homepage from "./pages/homepage/homepage.component";
+import { SearchProvider } from "./contexts/searchContext";
 
 function App() {
   return (
     <div className="App">
-      <Routes>
-        <Route path="/" element={<Homepage/>} />
-      </Routes>
+      <SearchProvider>
+        <Routes>
+          <Route path="/" element={<Homepage />} />
+        </Routes>
+      </SearchProvider>
     </div>
   );
 }
