f32e5ebebec51dd101039856bc59479d9ddae0b6
zacheusIsrael <segunzacheusi@gmail.com>
2023-02-17 20:29:56 +0100

    Update searchFilter.js

--- src/utilities/searchFilter.js
+++ src/utilities/searchFilter.js
@@ -1,16 +1,37 @@
+import genreList from "./genres";
+
+//search filter
 export const searchFilter = (searchKey, data) => {
-  let newData = data.filter(
-    (each) => each?.title.toLowerCase().indexOf(searchKey.toLowerCase()) === 0
-  );
+  let newData;
+  if (searchKey) {
+    newData = data.filter(
+      (each) => each?.title.toLowerCase().indexOf(searchKey.toLowerCase()) === 0
+    );
+  } else newData = data;
   return newData;
 };
 
+//filter by genre
+function getKeyByValue(object, value) {
+  return Object.keys(object).find((key) => object[key] === value);
+}
 export const genreFilter = (genreFilterKey, data) => {
-  let newData = data.filter((each) => each?.genre === genreFilterKey);
+  const genreId = getKeyByValue(genreList, genreFilterKey);
+  let newData = [];
+  if (genreFilterKey === "All") return data;
+  for (const movie of data) {
+    if (movie?.genre_ids.includes(parseInt(genreId))) {
+      newData.push(movie);
+    }
+  }
   return newData;
 };
 
+//filter by release date
 export const dateFilter = (releaseDate, data) => {
-  let newData = data.filter((each) => each?.date === releaseDate);
+  if (releaseDate === "All") return data;
+  const newData = data.filter(
+      (each) => each?.release_date.slice(0, 4) === releaseDate
+    );
   return newData;
 };
