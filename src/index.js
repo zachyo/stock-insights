b861b748e964aa8836d1a2f0ad78121e6d711bb4
zacheusIsrael <segunzacheusi@gmail.com>
2023-02-17 20:29:48 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -10,9 +10,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-      <SearchProvider>
         <App />
-      </SearchProvider>
     </BrowserRouter>
   </React.StrictMode>
 );
