1209d580615ff0b148f69804ec11297f8dd518a3
zacheusIsrael <segunzacheusi@gmail.com>
2023-02-22 01:25:52 +0100

    Update error.jsx

--- src/components/error/error.jsx
+++ src/components/error/error.jsx
@@ -1,7 +1,7 @@
-const Error = (error) => {
+const Error = ({error}) => {
   return (
     <div className="error">
-        {error.message}
+        {/* {error.message} */}
       <h1>Failed to fetch</h1>
       <p>Kindly reload the page or check your internet connection</p>
     </div>
