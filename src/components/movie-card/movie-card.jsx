f42fe014ffb2d88eb8395b8126cf7773da67d99e
zacheusIsrael <segunzacheusi@gmail.com>
2023-02-22 01:25:59 +0100

    Update movie-card.jsx

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -1,11 +1,9 @@
-import { Link } from "react-router-dom";
 import genreList from "../../utilities/genres";
 
 import "./movie-card.scss";
 
 const MovieCard = ({ movie }) => {
   const { title, poster_path, release_date, overview,genre_ids } = movie;
-  const name = `${movie.name}`;
   const release_year = release_date.slice(0, 4);
   const img_url = `https://image.tmdb.org/t/p/w500/${poster_path}`;
   const genres = genre_ids.map((id) => genreList[id]).map((genre,index) => {
@@ -18,7 +16,6 @@ const MovieCard = ({ movie }) => {
       <p>{overview}</p>
       <p>Genres : {genres}</p>
       <p>Released : {release_year}</p>
-      <Link to={`${name}`}>View movie info &rarr;</Link>
     </div>
   );
 };
